1075f2e7990bd84e9707d2e21adab73987b4b52d
Unknown <KasperBMaegaard@gmail.com>
2017-06-16 11:21:58 +0200

    Change in statistics

--- Data_satistics.py
+++ Data_satistics.py
@@ -1,2 +1,17 @@
 import numpy as np
 import pandas as pd
+
+def dataStatistics(data, statistic, Yref, Zref, DeltaX):
+
+
+    if statistic == "Mean":
+        result =
+
+    if statistic == "Variance":
+        result =
+
+    if statistic == "Cross correlation":
+        asgkjnasg
+        askgjnadg
+        result =
+    return result
