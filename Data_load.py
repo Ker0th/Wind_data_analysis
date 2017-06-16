b25ed7b99a0e08b1108a131eaea06ae1463748bf
Unknown <anders.slotsbo@gmail.com>
2017-06-16 11:40:06 +0200

    dataload

--- Data_load.py
+++ Data_load.py
@@ -1,7 +1,14 @@
 import numpy as np
-import pandas as pd
+from pandas import DataFrame
 
-def dataLoad(filename):
-    #
-    #
-    return data
+names = 'count', 'avg', 'scale'
+
+# note that the offsets are larger than the size of the type because of
+# struct padding
+offsets = 0, 8, 16
+formats = 'i4', 'f8', 'f4'
+dt = np.dtype({'names': names, 'offsets': offsets, 'formats': formats},
+              align=True)
+df = DataFrame(np.fromfile('test.bin', dt))
+
+print(df)
