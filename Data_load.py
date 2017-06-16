856d086366d7484b5e78c2a6a9ea78445ed2515b
Unknown <anders.slotsbo@gmail.com>
2017-06-16 12:21:15 +0200

    Dataload done ish

--- Data_load.py
+++ Data_load.py
@@ -1,14 +1,17 @@
 import numpy as np
 from pandas import DataFrame
 
-names = 'count', 'avg', 'scale'
+dt = np.zeros(3)
+def dataLoad(filename):
+    names = 'Nx', 'Ny', 'Nz'
 
-# note that the offsets are larger than the size of the type because of
-# struct padding
-offsets = 0, 8, 16
-formats = 'i4', 'f8', 'f4'
-dt = np.dtype({'names': names, 'offsets': offsets, 'formats': formats},
-              align=True)
-df = DataFrame(np.fromfile('test.bin', dt))
-
-print(df)
+    # note that the offsets are larger than the size of the type because of
+    # struct padding
+    offsets = 8, 4, 0
+    formats = 'f4', 'f4', 'f4'
+    dt = np.dtype({'names': names, 'offsets': offsets, 'formats': formats},align=True)
+    data = DataFrame(np.fromfile(filename, dt))
+    data = np.matrix(data)
+    print(dt)
+    return data
+print(dataLoad('test.bin'))
