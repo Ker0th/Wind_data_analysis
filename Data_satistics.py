b2bf50e78c2aac8556d5816580496ac145d5f382
Unknown <anders.slotsbo@gmail.com>
2017-06-19 11:27:31 +0200

    Menu added

--- Data_satistics.py
+++ Data_satistics.py
@@ -3,15 +3,15 @@ import pandas as pd
 
 def dataStatistics(data, statistic, Yref, Zref, DeltaX):
 
+    if statistic == " Mean":
+        result = np.mean(data,axis=0)
 
-    if statistic == "Mean":
-        #result =
+    if statistic == " Variance":
+        result = np.zeros((2,2))
 
-    if statistic == "Variance":
-        #result =
+    if statistic == " Cross correlation":
+        #asgkjnasg
+        #askgjnadg
+        result = np.zeros((2,2))
 
-    if statistic == "Cross correlation":
-        asgkjnasg
-        askgjnadg
-        #result =
     return result
