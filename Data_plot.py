ecc8b42a58ea12a83f89ed4270c5c9092d20842f
Unknown <mjungee12@hotmail.com>
2017-06-19 11:41:03 +0200

    Start of plot

--- Data_plot.py
+++ Data_plot.py
@@ -1,2 +1,20 @@
 import numpy as np
 import pandas as pd
+import matplotlib.pyplot as plt
+
+
+def dataPlot(data,statistic):
+
+    data=dataLoad("turb.bin")
+
+    if statistic == 'Mean':
+        plt.contourf(data)
+        plt.xlabel(y values)
+        plt.ylabel(z values)
+        plt.title(Plot plot)
+
+
+dataStat=dataStatistic.datastatistic(data,'Mean')
+
+
+    dataPlot('turb.bin',
