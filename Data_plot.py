8cf7ce2895fb3a73a55c8cd634115a71a6083885
Unknown <mjungee12@hotmail.com>
2017-06-19 13:19:30 +0200

    Data.plot

--- Data_plot.py
+++ Data_plot.py
@@ -1,20 +1,25 @@
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
-
+from Data_satistics import *
+from Data_load import *
 
 def dataPlot(data,statistic):
 
-    data=dataLoad("turb.bin")
+    data=dataLoad("test.bin",170,170,170)
+    result=dataStatistics(data,statistic,0,0,0)
 
-    if statistic == 'Mean':
-        plt.contourf(data)
-        plt.xlabel(y values)
-        plt.ylabel(z values)
-        plt.title(Plot plot)
+#Contourplot f statistic
+    cs=plt.contourf(result)
+
+    plt.colorbar(cs)
+    plt.xlabel('yvalues')
+    plt.ylabel('zvalues')
+    plt.title('{0} Value of wind data in the X-direction or Y-Z plane'.format(statistic))
+    plt.show()
+
+dataPlot(dataLoad("test.bin",20,20,20),' Mean')
 
 
-dataStat=dataStatistic.datastatistic(data,'Mean')
+#dataStat=dataStatistic.datastatistic(data,'Mean')
 
-
-    dataPlot('turb.bin',
