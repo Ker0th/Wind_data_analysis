caef7fc089529e31e305abea7d1a0cb8acefeb42
Unknown <KasperBMaegaard@gmail.com>
2017-06-20 15:36:07 +0200

    Comments for the statistics

--- Data_satistics.py
+++ Data_satistics.py
@@ -1,6 +1,16 @@
 import numpy as np
 
 def dataStatistics(data, statistic, Yref, Zref, DeltaX):
+    '''
+
+    :param data: An Nx x Ny x Nz array consisting of wind speed values
+    :param statistic: This is a string specifying the statistic that should be calculated (Mean, Variance or Cross correlation)
+    :param Yref: The reference y-coordinate, which is used only in the Cross correlation.
+    :param Zref: The reference z-coordinate, which is used only in the Cross correlation.
+    :param DeltaX: Seperation in the x-coordinate, for which the Cross correlation has to be evaluated
+    :return: An Ny x Nz array containing the calculated statistic for each point in the y-z plane.
+    '''
+
 
     if statistic == " Mean":
         result = np.mean(data, axis = 0)
@@ -9,9 +19,13 @@ def dataStatistics(data, statistic, Yref, Zref, DeltaX):
         result = np.var(data, axis = 0)
 
     elif statistic == " Cross correlation":
+
+        # Defining Nx as a constant
         Nx = data.shape[0]
-        a = data[:, Nx - DeltaX]
-        b = data[DeltaX:, Yref, Zref]
-        result = (np.sum(np.multiply(a, b[:, np.newaxis, np.newaxis]), axis = 0)) / (Nx - DeltaX)
+
+        # Creating two arrays to use in the calculation for the Cross correlation
+        DVec = data[:, Nx - DeltaX]
+        DrefVec = data[DeltaX:, Yref, Zref]
+        result = (np.sum(np.multiply(DVec, DrefVec[:, np.newaxis, np.newaxis]), axis = 0)) / (Nx - DeltaX)
 
     return result
